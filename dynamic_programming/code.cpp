4566a51d3f6bae6ba7f5806f0c38ae94b7377de7
superchd <63406434+superchd@users.noreply.github.com>
2022-04-02 15:32:38 +0900

    Update code.cpp
    revise but no answer

--- dynamic_programming/code.cpp
+++ dynamic_programming/code.cpp
@@ -1,26 +1,57 @@
 #include <iostream>
 #include <string>
-
-string  example;
-string  reverse;
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+string  A;
+string  B;
+
+int     LCS[1001][1001];
+
 void    my_input(void){
-    cin >> example;
-    reverse = example;
-    reverse.reverse(example.length());
+    cin >> A;
+    B = A;
+    reverse(B.begin(), B.end());
     return;
+}
+/*
+void    lcs_setting(void){
+    string temp;
+    string rev_temp;
+    temp = A;
+    rev_temp = B;
+
+    A.resize(A.length() + 1);
+    B.resize(B.length() + 1);
+
+    for(int i = 1; i < A.length(); i++){
+        A[i] = temp[i - 1];
+        B[i] = rev_temp[i - 1];
+    }
+
+    return ;
+}
+*/
+void    my_sol(void){
+    for (int i = 1; i < A.length(); i++){
+        for (int j = 1; j < B.length(); j++){
+            if (A[i - 1] == B[j - 1]){
+                LCS[i][j] = LCS[i - 1][j - 1] + 1;}
+            else {LCS[i][j] = max(LCS[i - 1][j] , LCS[i][j - 1]);}
+        }
+    }
+    cout << LCS[A.length()][B.length()];
+    return ;
 }
 
 
 int main(void){
 
     my_input();
-    cout << reverse << endl << example << endl;
-    return;
-
-
-
+   //lcs_setting();
+    my_sol();
+    return 0;
 
 }
